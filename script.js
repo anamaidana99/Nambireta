608bea6ab5dfeaa9d4ed785a1d7f2a36da57fa6b
anamaidana99 <139254226+anamaidana99@users.noreply.github.com>
2023-07-21 22:36:47 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,33 +1,36 @@
-// JavaScript code for "Cargar más" and "Mostrar menos" functionality
 
-// JavaScript code for "Cargar más" and "Mostrar menos" functionality
+  // JavaScript code for "Cargar más" and "Mostrar menos" functionality
 
-const allImages = document.querySelectorAll('.img-container');
-const buttonLoadMore = document.getElementById('button-load-more');
-let currentIndex = allImages.length - 1;
-let isShowingAll = false;
+  const allImages = document.querySelectorAll('.img-container');
+  const buttonLoadMore = document.getElementById('button-load-more');
+  let currentIndex = 0;
+  let isShowingAll = false;
 
-const showImages = () => {
-  if (!isShowingAll) {
-    allImages[currentIndex].style.display = 'block';
-    currentIndex--;
-    if (currentIndex < 0) {
-      buttonLoadMore.textContent = 'Mostrar menos';
-      isShowingAll = true;
+  const showImages = () => {
+    if (!isShowingAll) {
+      for (let i = currentIndex; i < currentIndex + 2 && i < allImages.length; i++) {
+        allImages[i].style.display = 'inline-block';
+      }
+      currentIndex += 2;
+      if (currentIndex >= allImages.length) {
+        buttonLoadMore.textContent = 'Mostrar menos';
+        isShowingAll = true;
+      }
+    } else {
+      allImages.forEach((img) => {
+        img.style.display = 'none';
+      });
+      buttonLoadMore.textContent = 'Cargar más';
+      isShowingAll = false;
       currentIndex = 0;
+      for (let i = currentIndex; i < currentIndex + 2 && i < allImages.length; i++) {
+        allImages[i].style.display = 'inline-block';
+      }
+      currentIndex += 2;
     }
-  } else {
-    allImages.forEach((img) => {
-      img.style.display = 'none';
-    });
-    buttonLoadMore.textContent = 'Cargar más';
-    isShowingAll = false;
-    currentIndex = allImages.length - 1;
-    allImages[currentIndex].style.display = 'block'; // Show the first image when resetting
-  }
-};
+  };
 
-buttonLoadMore.addEventListener('click', showImages);
+  buttonLoadMore.addEventListener('click', showImages);
 
-// Initial display: Show only the last image
-showImages();
+  // Initial display: Show the first 2 images
+  showImages();
